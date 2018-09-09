6135e784c9436af6b805856c76fabfaae49d316b
Shao Eric Hu <erichu11364@gmail.com>
2018-09-09 06:21:47 -0400

    Reworked some more of the frontend

--- host/hostapp/src/App.js
+++ host/hostapp/src/App.js
@@ -1,63 +1,75 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Helmet} from 'react-helmet';
+import { Helmet } from 'react-helmet';
 import {
   Stitch,
   UserPasswordCredential,
   RemoteMongoClient
-} from "mongodb-stitch-browser-sdk";
-import Card  from "./components/Card/Card";
+} from 'mongodb-stitch-browser-sdk';
+import Card from './components/Card/Card';
 import YouTube from 'react-youtube';
 import socketIOClient from 'socket.io-client';
+import logo from './logo.png';
 
-const socket = socketIOClient("http://localhost:4001");
+const socket = socketIOClient('http://localhost:4001');
 const client = Stitch.initializeDefaultAppClient('pennapps-mnfjh');
-const db = client.getServiceClient(RemoteMongoClient.factory, 'mongodb-atlas').db('data');
+const db = client
+  .getServiceClient(RemoteMongoClient.factory, 'mongodb-atlas')
+  .db('data');
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super();
-    this.state ={
-        songData: [],
-        currentSongName: "None being played",
-        currentSongId: ''
+    this.state = {
+      songData: [],
+      currentSongName: 'None being played',
+      currentSongId: ''
     };
     this.getSongs = this.getSongs.bind(this);
     this.updateCurrentSong = this.updateCurrentSong.bind(this);
     this.renderSongList = this.renderSongList.bind(this);
   }
-  updateCurrentSong = (song) => {
-      this.setState({currentSongName: song.name});
-      this.setState({currentSongId: song.id});
-  }
-  renderSongList(){
+  updateCurrentSong = song => {
+    this.setState({ currentSongName: song.name });
+    this.setState({ currentSongId: song.id });
+  };
+  renderSongList() {
     let songArr = this.state.songData;
-    songArr = songArr.sort(function(a,b){
-      if(a.vote_count > b.vote_count){
+    songArr = songArr.sort(function(a, b) {
+      if (a.vote_count > b.vote_count) {
         return -1;
-      } else if(a.vote_count < b.vote_count){
+      } else if (a.vote_count < b.vote_count) {
         return 1;
       } else {
-        return a.title > b.title ? -1:1;
+        return a.title > b.title ? -1 : 1;
       }
     });
-    return songArr.map((data,index) => <Card info={data} key={index} updateCurrentSong={this.updateCurrentSong}/>);
+    return songArr.map((data, index) => (
+      <Card
+        info={data}
+        key={index}
+        updateCurrentSong={this.updateCurrentSong}
+      />
+    ));
   }
-  getSongs(){
-     db.collection("playlist")
-    .find({}, {limit: 50})
-    .asArray()
-    .then(songs => this.setState({ songData: songs[0].songs}))
-    .then(this.renderSongList())
-    .catch(err => console.log(err));
+  getSongs() {
+    db.collection('playlist')
+      .find({}, { limit: 50 })
+      .asArray()
+      .then(songs => this.setState({ songData: songs[0].songs }))
+      .then(this.renderSongList())
+      .catch(err => console.log(err));
   }
 
-  componentDidMount(){
-    const credentials = new UserPasswordCredential("dj@pennapps.com","password123");
+  componentDidMount() {
+    const credentials = new UserPasswordCredential(
+      'dj@pennapps.com',
+      'password123'
+    );
     client.auth
-    .loginWithCredential(credentials)
-    .then(this.getSongs)
-    .catch(console.error);
+      .loginWithCredential(credentials)
+      .then(this.getSongs)
+      .catch(console.error);
     socket.on('update_ranking', this.getSongs);
   }
 
@@ -65,7 +77,7 @@ class App extends Component {
     const opts = {
       height: 700,
       width: 1100,
-      playerVars: { autoplay: 1}
+      playerVars: { autoplay: 1 }
     };
     return (
       <div className="App">
@@ -73,21 +85,38 @@ class App extends Component {
           <style>{'body{background-color: #1B9CFC;}'}</style>
         </Helmet>
         <div className="App-header">
-          <div style={{color: "white", fontSize: "2.5rem", marginTop: "20px", marginLeft: "10px"}}>Shuffle</div>
+          <img
+            src={logo}
+            style={{
+              height: '80px',
+              width: '80px',
+              marginTop: '10px',
+              marginLeft: '10px'
+            }}
+          />
+          <div
+            style={{
+              color: 'white',
+              fontSize: '2.5rem',
+              marginTop: '25px',
+              marginLeft: '10px'
+            }}
+          >
+            Shuffle
+          </div>
         </div>
-          <YouTube
-            className = "videoPlay"
-            videoId= {this.state.currentSongId}
-            opts={opts} />
+        <YouTube
+          className="videoPlay"
+          videoId={this.state.currentSongId}
+          opts={opts}
+        />
         <div className="leftSide">
           Currently Playing: {this.state.currentSongName}
         </div>
-        <div className="songChoice">
-          {this.renderSongList()}
-        </div>
+        <div className="songChoice">{this.renderSongList()}</div>
       </div>
     );
-  } 
+  }
 }
 
 export default App;
